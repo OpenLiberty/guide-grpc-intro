1fd8a3f6016b9c89e0e18244b79e808f3150c545
Jakub Pomykala <jakub.pomykala@hotmail.co.uk>
2022-12-16 17:49:43 +0000

    adding -ntp to maven

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -euxo pipefail
 
-mvn -pl systemproto -q clean install
+mvn -ntp -pl systemproto -q clean install
 
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -pl system -q clean package liberty:create liberty:install-feature liberty:deploy
 
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -pl query -q clean package liberty:create liberty:install-feature liberty:deploy
 
-mvn -pl system -ntp liberty:start
-mvn -pl query -ntp liberty:start
+mvn -ntp -pl system -ntp liberty:start
+mvn -ntp -pl query -ntp liberty:start
 
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -pl query -ntp failsafe:integration-test
 
-mvn -pl query -ntp failsafe:verify
+mvn -ntp -pl query -ntp failsafe:verify
 
-mvn -pl system -ntp liberty:stop
-mvn -pl query -ntp liberty:stop
+mvn -ntp -pl system -ntp liberty:stop
+mvn -ntp -pl query -ntp liberty:stop
