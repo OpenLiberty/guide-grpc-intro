92e60409ae309a6612ea9a6cdda60d348d777b80
Gilbert Kwan <gkwan@ca.ibm.com>
2023-03-08 15:12:16 -0500

    dev content review

--- finish/system/src/test/java/io/openliberty/guides/system/SystemServiceTest.java
+++ finish/system/src/test/java/io/openliberty/guides/system/SystemServiceTest.java
@@ -47,7 +47,7 @@ import io.openliberty.guides.systemproto.SystemServiceGrpc.SystemServiceStub;
 
 public class SystemServiceTest {
 
-	private static final String SERVER_NAME = "system";
+    private static final String SERVER_NAME = "system";
 	
     private static Server inProcessServer;
     private static ManagedChannel inProcessChannel;
